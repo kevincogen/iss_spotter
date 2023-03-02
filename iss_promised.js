0523952610623e3f2c284bb2f3a3eac80ada2e8a
Kevin Cogen <k.cogen@gmail.com>
2023-03-02 16:11:49 -0500

    Promise functionality added

--- iss_promised.js
+++ iss_promised.js
@@ -12,5 +12,13 @@ const fetchISSFlyOverTimes = function(body) {
   const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
   return request(url);
 };
-
-module.exports = {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes}
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+module.exports = {nextISSTimesForMyLocation, fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
